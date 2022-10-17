70dfc97beb6ce1b75a256bcca8b8cbe27be08415
elitesuper <tonylobbin7712@gmail.com>
2022-10-17 01:58:06 -0700

    view Responsive Design Ended

--- src/components/View/View.js
+++ src/components/View/View.js
@@ -13,14 +13,26 @@ const View = () => {
                         <img src={view}/ >
                     </div>
                     <div className='view__text'>
-                            <h2>View our Vehicles in Metaverse</h2>
+                            <h2>View our Vehicles in <span className="meta">Metaverse</span></h2>
                             <div className='view-mobile-flex'>
-                            <p>We offer you a unique opportunity to view the our cars<br/> in a Metaverse, so you are not buying a pig in a poke.<br/> The Metaverse are available for all the vehicles.</p>
+                            <p>We offer you a unique opportunity to view the our cars in a Metaverse, so you are not buying a pig in a poke. The Metaverse are available for all the vehicles.</p>
                             <Button className='view__btn' variant="contained" endIcon={<ArrowRightAltIcon />}>
                               Explore Now
                             </Button>
                         </div>
-                       
+                    </div>
+                </div>
+                <div className='view-hero-2 d-flex justify-content-center'>
+                    <div className='view__text-2'>
+                        <h2>View our Vehicles in <span className="meta">Metaverse</span></h2>
+                        <div className='view-mobile-flex'>
+                            <p>We offer you a unique opportunity to view the our cars in a Metaverse, so you are not buying a pig in a poke. The Metaverse are available for all the vehicles.</p>
+                            <div className="row justify-content-center">
+                                <Button className='view__btn' variant="contained" endIcon={<ArrowRightAltIcon />}>
+                                    Explore Now
+                                </Button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </Container>
